d8c1070e74f80e36dcba039832271cb4b3a4c7ec
yizhou <1394853556@qq.com>
2020-07-23 13:30:17 +0800

    webpack config

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -90,6 +90,8 @@ module.exports = {
         historyApiFallback: {
             disableDotRule: true,
         },
+        // host: '0.0.0.0',
+        port: '3000',
         watchOptions: {
             ignored: /node_modules/,
         },
