40d01992de1570e170e2292e5b8b6829b8980bf6
yizhou <1394853556@qq.com>
2020-07-23 13:25:46 +0800

    api.js

--- src/js/api.js
+++ src/js/api.js
@@ -2,44 +2,94 @@ import axios from 'axios';
 
 export default {
     send: (options) => {
-        axios
-            .post(options.url, options.data)
-            .then((response) => {
-                const data = response.data;
-                if (!data || data.code !== 0) {
-                    options.error && options.error(data && data.msg);
-                    return;
+        console.log(options.data)
+        fetch('https://qa.sendawish.net/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': "*",
+                'Access-Control-Allow-Headers': '*'
+            },
+            mode: 'cors',
+            body: JSON.stringify({
+                query: `
+                mutation ($input: DanmakuTextInput!) {
+                    createDanmakuText(input: $input) {
+                        id
+                        showtimeInSecond
+                        transition
+                        color
+                        creator {
+                            id
+                            fullname
+                        }
+                        body
+                    }
+                }`,
+                variables: {
+                    "input": options.data
                 }
-                options.success && options.success(data);
+            }),
+        })
+            .then(res => res.json())
+            .then(res => {
+                console.log('AL: res', res)
             })
-            .catch((e) => {
-                console.error(e);
+            .catch(err => {
+                console.error(err);
                 options.error && options.error();
             });
     },
 
     read: (options) => {
-        axios
-            .get(options.url)
-            .then((response) => {
-                const data = response.data;
-                if (!data || data.code !== 0) {
-                    options.error && options.error(data && data.msg);
-                    return;
+        fetch(options.url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': "*",
+                'Access-Control-Allow-Headers': '*'
+            },
+            mode: 'cors',
+            body: JSON.stringify({
+                query: `
+                query danmakuQuery($id: String!, $token: String!) {
+                    thingInvite(id: $id, token: $token) {
+                        id
+                        danmakus {
+                            id
+                            showtimeInSecond
+                            transition
+                            color
+                            body
+                            creator {
+                                id
+                                email
+                                fullname
+                            }
+                        }
+                    }
+                }`,
+                variables: {
+                    ...options.data
                 }
-                options.success &&
-                    options.success(
-                        data.data.map((item) => ({
-                            time: item[0],
-                            type: item[1],
-                            color: item[2],
-                            author: item[3],
-                            text: item[4],
-                        }))
-                    );
+            }),
+        })
+            .then(res => res.json())
+            .then(res => {
+                options.success && options.success(
+                    res.data.thingInvite.danmakus.map(d =>
+                        ({
+                            time: d.showtimeInSecond,
+                            type: d.transition,
+                            color: d.color,
+                            author: d.creator.id,
+                            text: d.body,
+                        })
+                    )
+                )
             })
-            .catch((e) => {
-                console.error(e);
+            .catch(err => {
+                console.error(err);
                 options.error && options.error();
             });
     },
