cc454687020ce867c659dcb4185cf4627a898e08
yizhou <1394853556@qq.com>
2020-08-03 16:56:31 +0800

    fix focus&hover

--- src/js/comment.js
+++ src/js/comment.js
@@ -103,14 +103,14 @@ class Comment {
     show() {
         this.player.controller.disableAutoHide = true;
         // this.player.template.controller.classList.add('dplayer-controller-comment');
-        this.player.template.mask.classList.add('dplayer-mask-show');
+        // this.player.template.mask.classList.add('dplayer-mask-show');
         // this.player.container.classList.add('dplayer-show-controller');
-        this.player.template.commentInput.focus();
+        // this.player.template.commentInput.focus();
     }
 
     hide() {
         // this.player.template.controller.classList.remove('dplayer-controller-comment');
-        this.player.template.mask.classList.remove('dplayer-mask-show');
+        // this.player.template.mask.classList.remove('dplayer-mask-show');
         // this.player.container.classList.remove('dplayer-show-controller');
         this.player.controller.disableAutoHide = false;
         this.hideSetting();
