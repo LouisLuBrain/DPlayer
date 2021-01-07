8d7074326755afa0a91a2ff85c49da74c8f9fbee
yizhou <1394853556@qq.com>
2021-01-07 14:50:00 +0800

    fix resize event

--- src/js/template.js
+++ src/js/template.js
@@ -33,7 +33,7 @@ class Template {
         this.container.innerHTML = utils.isMobile ? mobilePlayer(templateOptions) : tplPlayer(templateOptions);
 
         window.addEventListener("resize", () => {
-            this.container.innerHTML = utils.isMobile() ? mobilePlayer(templateOptions) : tplPlayer(templateOptions);
+            this.container.innerHTML = window.innerWidth <= 768 ? mobilePlayer(templateOptions) : tplPlayer(templateOptions);
         });
 
         this.volumeBar = this.container.querySelector('.dplayer-volume-bar-inner');
